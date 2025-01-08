3e3a0e4079a6754df370ac117be0fad1a1d847bd
Adebanjo15 <lekanjackson461@gmail.com>
2025-01-08 10:05:48 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349035615458"
+global.pairingNumber = "2349036039811"
 
-global.botNumber = ["2349035615458"]
+global.botNumber = ["2349036039811"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
@@ -20,9 +20,9 @@ global.autoBio = true
 global.antiSpam = true
 global.sign = "☃️❄️"
 // ☞ ➻ ➽ ☛ 〆 
-global.bank = "Opay"
-global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.bank = "MONIEPOINT"
+global.bankname = "OLUWOLE"
+global.accnumber = "7042850798"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
